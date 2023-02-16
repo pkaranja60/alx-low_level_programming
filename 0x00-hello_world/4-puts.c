20f3c0e1b8095965f2d1c81878f010d9a70c0bc4
pkaranja60 <peterkaranja60@gmail.com>
2023-02-16 13:17:50 -0800

    learning C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - a program that prints a line with function
+ *
+ */
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
